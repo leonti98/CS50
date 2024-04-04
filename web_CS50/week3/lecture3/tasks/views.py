9d9a995d9b985a22794245896eec71368f1fa4a1
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-04-04 18:58:09 +0400

    create layout template

--- web_CS50/week3/lecture3/tasks/views.py
+++ web_CS50/week3/lecture3/tasks/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+tasks = ["foo", "bar", "baz"]
+
 # Create your views here.
 def index(request):
-    return render(request, 'tasks/index.html')
+    
+    return render(request, 'tasks/index.html', {
+        "tasks": tasks
+    })
+    
+def add(request):
+    return render(request, "tasks/add.html")
