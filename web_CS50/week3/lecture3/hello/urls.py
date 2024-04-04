05cdc9bb82b7247269a27454ee135da1ee5bbe66
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-04-04 15:31:31 +0400

    add urls

--- web_CS50/week3/lecture3/hello/urls.py
+++ web_CS50/week3/lecture3/hello/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("levan", views.levan, name="levan"),
+    path("david", views.david, name="david"),
 ]
--- web_CS50/week3/lecture3/hello/views.py
+++ web_CS50/week3/lecture3/hello/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello!")
+
+def levan(request):
+    return HttpResponse("Hello, Levan")
+
+def david(request):
+    return HttpResponse("Hello, David")
