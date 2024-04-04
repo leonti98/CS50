20b3bdc7e6bb73f13e99ac5a29dc3a1f17485098
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-04-04 15:25:31 +0400

    bug fix

--- web_CS50/week3/lecture3/hello/views.py
+++ web_CS50/week3/lecture3/hello/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(reques):
-    return HttpResponse("Hello, world!")
+def index(request):
+    return HttpResponse("Hello!")
