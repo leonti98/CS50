5e9a6c6e9721e53ea82a1567eaecd8a1396d3965
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-04-05 01:22:05 +0400

    django server-side form validation

--- web_CS50/week3/lecture3/tasks/views.py
+++ web_CS50/week3/lecture3/tasks/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 tasks = ["foo", "bar", "baz"]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 # Create your views here.
 def index(request):
@@ -20,6 +21,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form": form
@@ -27,3 +29,4 @@ def add(request):
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
+    
