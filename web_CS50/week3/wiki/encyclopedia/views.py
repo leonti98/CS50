324fc104b77f71ac31e7565be3cc2d0c53741a03
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-04-08 23:18:04 +0400

    Display entry content

--- web_CS50/week3/wiki/encyclopedia/views.py
+++ web_CS50/week3/wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 from . import util
 
@@ -8,3 +9,21 @@ def index(request):
         "entries": util.list_entries()
     })
 
+# def text(request, text):
+#     entries = util.list_entries
+#     return render(request, "encyclopedia/test.html", {
+#         "entries": entries
+#     })
+    
+def entry_page(request, entry_title):
+    entry_content = util.get_entry(entry_title)
+    if entry_content:
+        return render(request, "encyclopedia/entry.html", {
+            "entry_content": entry_content
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "entry_title": entry_title
+        })
+    
+    
