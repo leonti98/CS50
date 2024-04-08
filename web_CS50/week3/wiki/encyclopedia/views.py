26ef45e8ba58684348c9646a2089e1fd1708e002
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-04-08 23:36:28 +0400

    list to links index page

--- web_CS50/week3/wiki/encyclopedia/views.py
+++ web_CS50/week3/wiki/encyclopedia/views.py
@@ -8,12 +8,6 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
-# def text(request, text):
-#     entries = util.list_entries
-#     return render(request, "encyclopedia/test.html", {
-#         "entries": entries
-#     })
     
 def entry_page(request, entry_title):
     entry_content = util.get_entry(entry_title)
