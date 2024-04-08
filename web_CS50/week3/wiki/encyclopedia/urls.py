94009b04713376a920f3e1bec34986096a5d6fcf
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-04-09 01:45:13 +0400

    sidebar search implementation

--- web_CS50/week3/wiki/encyclopedia/urls.py
+++ web_CS50/week3/wiki/encyclopedia/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry_title>", views.entry_page, name="entry_page"),
+    path("search", views.search, name="search"),
+    path("suggestions", views.suggestions, name="suggetions")
 ]
--- web_CS50/week3/wiki/encyclopedia/views.py
+++ web_CS50/week3/wiki/encyclopedia/views.py
@@ -13,6 +13,7 @@ def entry_page(request, entry_title):
     entry_content = util.get_entry(entry_title)
     if entry_content:
         return render(request, "encyclopedia/entry.html", {
+            "entry_title": entry_title,
             "entry_content": entry_content
         })
     else:
@@ -20,4 +21,25 @@ def entry_page(request, entry_title):
             "entry_title": entry_title
         })
     
-    
+def search(request):
+    link = request.GET
+    get_str = link["q"]
+    list = util.list_entries()
+    in_list = [x for x in list if x.lower() == get_str.lower()]
+    if in_list:
+        return redirect(f"wiki/{in_list[0]}")
+    else:
+        matching = [s for s in list if get_str.lower() in s.lower()]
+        if matching:
+            print(matching)
+            return render(request, "encyclopedia/suggestions.html", {
+                "matching": matching
+            })
+        else:
+            print("NO MATCH")
+            return render(request, "encyclopedia/error.html", {
+            "entry_title": get_str
+        })
+        
+def suggestions(request):
+    return render(request, "suggestions.html")
