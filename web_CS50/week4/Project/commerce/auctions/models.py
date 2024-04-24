02845d34ec2ed4ea9cef73f3bbacbca508a60573
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-04-24 18:23:20 +0400

    Started creating models

--- web_CS50/week4/Project/commerce/auctions/models.py
+++ web_CS50/week4/Project/commerce/auctions/models.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.fields 
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Product(models.Model):
+    title - models.CharField(max_length=20)
+    description = models.TextField(max_length=400)
+    models.ImageField(
+        _(""), upload_to=None, height_field=None, width_field=None, max_length=None
+    )
+    sratring_price = models.DecimalField(max_digits=6, decimal_places=2)
+    sratring_price = models.
