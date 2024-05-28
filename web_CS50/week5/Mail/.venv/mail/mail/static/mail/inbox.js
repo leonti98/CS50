6ff146e931daca4ff2a6e183effe288d0de4651d
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-05-28 05:02:47 +0400

    Finished

--- web_CS50/week5/Mail/.venv/mail/mail/static/mail/inbox.js
+++ web_CS50/week5/Mail/.venv/mail/mail/static/mail/inbox.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     .querySelector('#sent')
     .addEventListener('click', () => load_mailbox('sent'));
   document
-    .querySelector('#archived')
+    .querySelector('#archive')
     .addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
@@ -34,9 +34,6 @@ function load_mailbox(mailbox) {
   fetch(`/emails/${mailbox}`)
     .then((response) => response.json())
     .then((emails) => {
-      // Print emails
-      console.log(emails);
-
       // Get the emails table element
       const emailsTable = document.querySelector('#emails-table');
       // Clear any existing content in the emails table
@@ -44,54 +41,87 @@ function load_mailbox(mailbox) {
         mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
       }</h3>`;
 
-      // ... do something else with emails ...
+      // Load latest email to in content view if mailbox is not empty
+      if (emails[0]) {
+        document.querySelector('#email-content').innerHTML = `
+                        <div class="p-4">
+                            <div class="btn-toolbar mb-3 d-flex justify-content-between"
+                                role="toolbar"
+                                aria-label="Toolbar with button groups">
+                                <div class="btn-group me-2" role="group" aria-label="First group" id="toolbar">
+                                </div>
+                            </div>
+                            <p class="text-muted mb-1">${emails[0].timestamp}</p>
+                            <h5 class="mb-3">${emails[0].subject}</h5>
+                            <div>
+                                <p class="mb-0">
+                                    <span class="text-muted">From :</span> ${emails[0].sender}
+                                </p>
+                                <p>
+                                    <span class="text-muted">To :</span> ${emails[0].recipients}
+                                </p>
+                            </div>
+                            <div>
+                                ${emails[0].body}
+                            </div>
+                        </div>`;
+        // Add reply and archive buttons
+        const toolbar = document.getElementById('toolbar');
+        if (mailbox != 'sent') {
+          const archiveButton = createArchiveButton(emails[0]);
+          toolbar.appendChild(archiveButton);
+          const replyButton = createReplyButton(emails[0]);
+          toolbar.appendChild(replyButton);
+        }
+      } else {
+        document.querySelector('#emails-table').innerHTML += `
+          <div class="alert alert-warning" role="alert">
+          ${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)} is empty
+        </div>`;
+        document.querySelector('#email-content').innerHTML = ``;
+      }
+      // Change navlik of current view to active
+      document
+        .querySelector('.nav-link.text-start.active')
+        .classList.remove('active');
+      document.querySelector(`#${mailbox}`).classList.add('active');
+      // For each email add list item to emails-table
       emails.forEach((element) => {
         // Create a list item for each email
         const li = document.createElement('li');
+
         // change backround based on if email was read
         if (element.read == true) {
           li.className = 'list-group-item shadow-sm rounded single-email mb-3';
         } else {
           li.className =
-            'list-group-item shadow-sm rounded single-email mb-3 active';
+            'list-group-item shadow-sm rounded single-email mb-3 bg-white';
         }
 
         li.innerHTML = `<div class="row">
-                            <div class="col-sm-6">
-                                <div>
-                                    <p class="mb-1 text-singleline text-dark">${element.sender}</p>
-                                    <p class="text-muted mb-0 text-singleline">${element.subject}</p>
+                            <div class="col-sm-6 d-flex flex-column">
+                                <div class='d-flex flex-column flex-grow-1 justify-content-around'>
+                                    <p class="mb-1 text-singleline">${element.sender}</p>
+                                    <p class="mb-0 fs-6 text-singleline">${element.subject}</p>
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="icons">
-                                    <p class="mb-1 text-muted">${element.timestamp}</p>
+                                    <p class="mb-1">${element.timestamp}</p>
                                     <a class="nav-link mb-0 text-singleline">
-                                        <i class="fa-solid fa-box-archive"></i>
                                     </a>
                                 </div>
                             </div>
                         </div>`;
 
-        // Add event listener to the email item
+        // Add event listener to the list item
         li.addEventListener('click', () => {
-          // Remove 'active' class from all items
-          document.querySelectorAll('li').forEach((item) => {
-            item.classList.remove('active');
-          });
-
           document.querySelector('#email-content').innerHTML = `
                         <div class="p-4">
                             <div class="btn-toolbar mb-3 d-flex justify-content-between"
                                 role="toolbar"
                                 aria-label="Toolbar with button groups">
-                                <div class="btn-group me-2" role="group" aria-label="First group">
-                                    <button type="button" class="btn text-muted">
-                                        <i class="fa-solid fa-reply"></i>
-                                    </button>
-                                    <button type="button" class="btn text-muted">
-                                      <i class="fa-solid fa-box-archive"></i>
-                                    </button>
+                                <div class="btn-group me-2" role="group" aria-label="First group" id="toolbar">
                                 </div>
                             </div>
                             <p class="text-muted mb-1">${element.timestamp}</p>
@@ -109,10 +139,22 @@ function load_mailbox(mailbox) {
                             </div>
                         </div>`;
 
-          // Add classname to make visible
-          li.classList.add('active');
+          // Remove class from active item
+          const liBackground = document.querySelector('.bg-blue-300');
+          if (liBackground) {
+            if (liBackground.classList.contains('bg-white')) {
+              liBackground;
+            }
+            liBackground.classList.remove('bg-blue-300', 'text-white');
+          }
+          // Remove backround if mail was not read
+          if (li.classList.contains('bg-white')) {
+            li.classList.remove('bg-white');
+          }
+          // Add classname to make selected email visible
+          li.classList.add('bg-blue-300', 'text-white');
 
-          // If mail is not read. mark it as read
+          // If mail is not read => mark it as read
           if (element.read == false) {
             element.read = true;
             fetch(`/emails/${element.id}`, {
@@ -122,11 +164,38 @@ function load_mailbox(mailbox) {
               }),
             });
           }
+
+          // Content-view toolbar
+          const toolbar = document.getElementById('toolbar');
+
+          if (mailbox != 'sent') {
+            // Create archive button
+            const archiveButton = createArchiveButton(element);
+            toolbar.appendChild(archiveButton);
+            // Create reply button
+            const replyButton = createReplyButton(element);
+            toolbar.appendChild(replyButton);
+          }
         });
 
         // Append the list item to the emails table
         emailsTable.appendChild(li);
+        archiveSpot = li.querySelector('a');
+        archiveSpot.appendChild(createArchiveButton(element));
       });
+
+      // Mark first element as active and read
+      const firstListItem = document.querySelector('ul > li');
+      firstListItem.classList.remove('bg-white', 'text-black');
+      firstListItem.classList.add('bg-blue-300', 'text-white');
+      if (emails[0].read == false) {
+        fetch(`/emails/${emails[0].id}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+            read: true,
+          }),
+        });
+      }
     });
 }
 
@@ -146,12 +215,86 @@ addEventListener('DOMContentLoaded', () => {
     })
       .then((response) => response.json())
       .then((result) => {
-        // Print result
         console.log(result);
         load_mailbox('sent');
-        document.querySelector('#email-content').innerHTML = 'hi';
       });
 
     return false;
   };
 });
+
+function createReplyButton(email) {
+  // Create reply Button
+  const replyButton = document.createElement('button');
+  replyButton.innerHTML = '<i class="fa-solid fa-reply"></i>';
+  replyButton.className = 'btn btn-primary';
+  replyButton.id = 'replyButton';
+  replyButton.value = `${email.id}`;
+  replyButton.onclick = reply;
+
+  return replyButton;
+}
+
+function createArchiveButton(email) {
+  // Create reply Button
+  const archiveBTN = document.createElement('button');
+  // Add inner html
+  archiveBTN.innerHTML = '<i class="fa-solid fa-box-archive fa-xs">';
+  archiveBTN.value = `${email.id}`;
+  // change btn background based on state
+  if (email.archived == false) {
+    archiveBTN.className = 'btn btn-primary archiveButton';
+  } else {
+    archiveBTN.className = 'btn btn-warning archiveButton';
+  }
+  // onclick functions to change state of archive
+  archiveBTN.onclick = archive;
+
+  return archiveBTN;
+}
+
+// Function to add to archive
+function archive() {
+  const archiveButton = document.querySelector('.archiveButton');
+  if (archiveButton.className.includes('btn-primary')) {
+    fetch(`/emails/${archiveButton.value}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        archived: true,
+      }),
+    }).then(() => {
+      load_mailbox('inbox');
+    });
+  } else {
+    fetch(`/emails/${archiveButton.value}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        archived: false,
+      }),
+    }).then(() => {
+      load_mailbox('archive');
+    });
+  }
+}
+
+function reply() {
+  // Get email id from button value
+  replyButton = document.getElementById('replyButton');
+  compose_email();
+  // Fetch email to reply
+  fetch(`/emails/${replyButton.value}`)
+    .then((response) => response.json())
+    .then((email) => {
+      // Change input field values
+      document.querySelector('#compose-recipients').value = `${email.sender}`;
+      document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
+      document.querySelector(
+        '#compose-body'
+      ).value = `On ${email.timestamp} ${email.sender} wrote: \n${email.body} \n\nReply: \n`;
+    });
+  // Swtich to compose tab
+  document
+    .querySelector('.nav-link.text-start.active')
+    .classList.remove('active');
+  document.querySelector('#compose').classList.add('active');
+}
