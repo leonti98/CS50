8c53b158c13892945ca0ba636d9528e7662476fb
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-07 13:56:37 +0400

    Changed yahoo api for CS50X Week 9

--- CS50X/Week9_Flask/finance/helpers.py
+++ CS50X/Week9_Flask/finance/helpers.py
@@ -1,9 +1,4 @@
-import csv
-import datetime
-import pytz
 import requests
-import urllib
-import uuid
 
 from flask import redirect, render_template, session
 from functools import wraps
@@ -52,35 +47,21 @@ def login_required(f):
 
 def lookup(symbol):
     """Look up quote for symbol."""
-
-    # Prepare API request
-    symbol = symbol.upper()
-    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
-    start = end - datetime.timedelta(days=7)
-
-    # Yahoo Finance API
-    url = (
-        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
-        f"?period1={int(start.timestamp())}"
-        f"&period2={int(end.timestamp())}"
-        f"&interval=1d&events=history&includeAdjustedClose=true"
-    )
-
-    # Query API
+    url = f"https://finance.cs50.io/quote?symbol={symbol.upper()}"
     try:
-        response = requests.get(
-            url,
-            cookies={"session": str(uuid.uuid4())},
-            headers={"Accept": "*/*", "User-Agent": "python-requests"},
-        )
-        response.raise_for_status()
-
-        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
-        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
-        price = round(float(quotes[-1]["Adj Close"]), 2)
-        return {"price": price, "symbol": symbol}
-    except (KeyError, IndexError, requests.RequestException, ValueError):
-        return None
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for HTTP error responses
+        quote_data = response.json()
+        return {
+            "name": quote_data["companyName"],
+            "price": quote_data["latestPrice"],
+            "symbol": symbol.upper()
+        }
+    except requests.RequestException as e:
+        print(f"Request error: {e}")
+    except (KeyError, ValueError) as e:
+        print(f"Data parsing error: {e}")
+    return None
 
 
 def usd(value):
